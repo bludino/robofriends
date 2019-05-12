ba5d233231069a97e6796fbe63f31a9fce71b3e1
Phil <bludino@users.noreply.github.com>
2019-05-12 08:48:34 +0100

    Updates

---
+++ precache-manifest.ff24f08a40a99b2d5808895d0d13c990.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47751b88ee9f6f17f9432713dea85679",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "8d8ad546c8bba8b27b11",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "4589ba791a7be8c01552",
+    "url": "/robofriends/static/css/main.c8d2aacf.chunk.css"
+  },
+  {
+    "revision": "8d8ad546c8bba8b27b11",
+    "url": "/robofriends/static/js/2.c05ccf0c.chunk.js"
+  },
+  {
+    "revision": "4589ba791a7be8c01552",
+    "url": "/robofriends/static/js/main.22276b3c.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
